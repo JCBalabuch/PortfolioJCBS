92d8f771e434d29a46fb0abe9893faeb3c04cdbc
JCBalabuch <balabuchj@gmail.com>
2023-07-28 22:39:00 -0400

    adjust some logos and features

--- components/ProjectsGallery/ProjectsGallery.js
+++ components/ProjectsGallery/ProjectsGallery.js
@@ -29,11 +29,6 @@ const projectsCards = (projects) => {
 
     const ulTechs = document.createElement("ul");
 
-    // console.log(project.projectTechs);
-
-    // const techList = project.projectTechs;
-    // const technologies = TECHS;
-
     project.projectTechs.forEach((tech) => {
       const liTech = document.createElement("li");
 
@@ -41,11 +36,6 @@ const projectsCards = (projects) => {
       techDiv.classList.add("techDiv");
 
       const techImg = document.createElement("img");
-
-      //   if (tech === technologies.techName) {
-      //     techImg.src = technologies.techLogo;
-      //     techImg.alt = technologies.techName;
-      //   }
 
       techImg.src = TECHS[tech];
       techImg.alt = tech;
@@ -60,8 +50,8 @@ const projectsCards = (projects) => {
     projectLinks.classList.add("projectLinks");
 
     const deploymentLink = document.createElement("a");
-    deploymentLink.src = project.projectURL;
-    deploymentLink.alt = project.projectName;
+    deploymentLink.href = project.projectURL;
+    deploymentLink.setAttribute("target", "_blank");
 
     const linkLogo = document.createElement("img");
     linkLogo.src = "LinkLogo.png";
@@ -69,8 +59,8 @@ const projectsCards = (projects) => {
     deploymentLink.appendChild(linkLogo);
 
     const githubLink = document.createElement("a");
-    githubLink.src = project.projectGithub;
-    githubLink.alt = project.projectName;
+    githubLink.href = project.projectGithub;
+    githubLink.setAttribute("target", "_blank");
 
     const linkGithub = document.createElement("img");
     linkGithub.src = "GitHubLogo.png";
@@ -85,7 +75,6 @@ const projectsCards = (projects) => {
     divProjectCard.appendChild(divProjectImg);
     divProjectCard.appendChild(projectTechs);
     divProjectCard.appendChild(projectLinks);
-
 
     liProjectCard.appendChild(divProjectCard);
     ulProjectCards.appendChild(liProjectCard);
@@ -103,59 +92,3 @@ export const projectsGallery = () => {
     </div>
       `;
 };
-
-// const projectsCards = (projects) => {
-//     const projectsGallery = document.createElement("div");
-//     projectsGallery.id = "projectsGallery";
-//     projectsGallery.classList = "projectsGallery";
-
-//     const ulProjectCards = document.createElement("ul");
-
-//     projects.forEach((project) => {
-//       const liProjectCard = document.createElement("li");
-
-//       const divProjectCard = document.createElement("div");
-//       divProjectCard.classList.add("divProjectCard");
-
-//       const divProjectImg = document.createElement("div");
-//       divProjectImg.classList.add("divProjectImg");
-//       const imgProject = document.createElement("img");
-//       imgProject.src = project.projectImg;
-//       imgProject.alt = project.projectImg;
-
-//       const projectCardTitle = document.createElement("h4");
-//       projectCardTitle.textContent = project.projectName;
-
-//       const projectTechs = document.createElement("div");
-//       projectTechs.classList.add("projectTechs");
-
-//       const ulTechs = document.createElement("ul");
-
-//       project.projectTechs.forEach((tech) => {
-//         const liTech = document.createElement("li");
-
-//         const techDiv = document.createElement("div");
-//         techDiv.classList.add("techDiv");
-
-//         const techImg = document.createElement("img");
-
-//         techImg.src = TECHS[tech];
-//         techImg.alt = tech;
-
-//         techDiv.appendChild(techImg);
-//         liTech.appendChild(techDiv);
-//         ulTechs.appendChild(liTech);
-//         projectTechs.appendChild(ulTechs);
-//       });
-
-//       divProjectImg.appendChild(imgProject);
-//       divProjectCard.appendChild(divProjectImg);
-//       divProjectCard.appendChild(projectCardTitle);
-
-//       liProjectCard.appendChild(divProjectCard);
-//       ulProjectCards.appendChild(liProjectCard);
-//     });
-
-//     projectsGallery.appendChild(ulProjectCards);
-//     return projectsGallery;
-//   };
